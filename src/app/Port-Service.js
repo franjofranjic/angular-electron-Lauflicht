1de1b35bc6bb0bd94cce07f634c92ec4d97465d6
franjofranjic <franjo.franjic@edu.gbssg.ch>
2020-04-25 13:02:30 +0200

    Electron Project v1

--- src/app/Port-Service.js
+++ src/app/Port-Service.js
@@ -2,13 +2,16 @@
 
 // Open event example
 const SerialPort = require('serialport')
+// Port auswählen
 const port = new SerialPort('/dev/tty-usbserial1', baudRate:9600)
 
 port.on('open', () => {
   console.log('Port Opened')
 })
 
-port.write('main screen turn on', err => {
+var data = "1;0;0;0;4000;";
+
+port.write(data, err => {
   if (err) {
     return console.log('Error: ', err.message)
   }
