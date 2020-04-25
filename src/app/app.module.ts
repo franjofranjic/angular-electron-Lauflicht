eb65dc074ba0946589374ec1395b2c17da2418f3
franjofranjic <franjo.franjic@edu.gbssg.ch>
2020-04-25 12:45:33 +0200

    Electron Project v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ControllerComponent } from './controller/controller.component';
+import { OutputComponent } from './output/output.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatSelectModule, MatSliderModule} from '@angular/material';
+import {NgbButtonsModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ControllerComponent,
+    OutputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatSelectModule,
+    MatButtonModule,
+    NgbButtonsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
